e60e8dad349bb8a65d21de01c928a92a6840fb39
Gustav.Nordlander <Gustav.nordlander@elev.ga.ntig.se>
2025-02-05 15:17:36 +0100

    börjat på menyn

--- Projekt prog 1/Program.cs
+++ Projekt prog 1/Program.cs
@@ -1,2 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// alla jävla kriteriers program i olika metoder
+static void Hej()
+{
+  Console.WriteLine("Hej!");
+}
+static void inut()
+{
+    string input = Console.ReadLine();
+    Console.WriteLine(input);
+}
+
+
+
+
+
+// Menyn för att välja alternativ
+bool isRunning = true;
+while (isRunning)
+{
+    Console.WriteLine(@"
+    Detta är en lista över våra betygs kriterier
+    skriv in nummret eller namnet på den för att köra en demo verision av ett program
+    som innhålelr de specifika koden
+
+1(Print)
+2(input-print) 
+    ");
+
+    string input = Console.ReadLine();
+    if(input == "1")
+    {
+        Hej();
+    }
+    else if(input == "2")
+    {
+        inut();
+    }
+
+   Console.ReadLine(); 
+}
